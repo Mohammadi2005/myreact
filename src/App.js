c9a41ac73f3bfbdaa06e62a0898da1ed5d0cc7c1
amir hossein <amohammadi161183@gmail.com>
2025-07-13 10:08:38 +0330

    use Reducer 1

--- src/App.js
+++ src/App.js
@@ -1,54 +1,33 @@
 import style from './App.module.css';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {useToggle} from "./customHooks/useToggle";
-import {useCat} from "./customHooks/useCat";
-import {useCount} from "./customHooks/useCount";
+import {useReducer} from "react";
+import {initialState, handleState} from "./useReducerHook/useReducer";
+import axios from "axios";
 
+function App() {
 
-const client = new QueryClient({
-    defaultOptions : {
-        queries: {
-            refetchOnWindowFocus: false,
-        }
+    const [state, dispatch] = useReducer(handleState, initialState)
+
+    const handleStateChange = () => {
+        dispatch({type: "fetch_start"})
+        axios.get("https://catfact.ninja/fact")
+            .then(res => {
+                dispatch({type: "fetch_success", fact: res.data.fact})
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch({type: "fetch_error"})
+            })
     }
-})
-
-function AppContent() {
-    const [state, toggle] = useToggle(true);
-    const { data: catData, isLoading, refetchData } = useCat();
-    const {count, increaseCount, decreaseCount, resetCount} = useCount();
-
 
     return (
         <div className={style.App}>
-            <div style={{ background: "#111561" }}>
-                <h1>hook 1 (useToggle)</h1>
-                <button onClick={toggle}>{state ? "Hidden" : "Show"}</button>
-                {state && <h2>this is my hook2</h2>}
-            </div>
-            <br />
-            <div style={{ background: "#155111" }}>
-                <h1>hook 2 (useCat)</h1>
-                {isLoading && <h2>loading ... </h2>}
-                <h2>{catData?.fact}</h2>
-                <button onClick={refetchData}>refetch Data</button>
-            </div>
-            <div style={{ background: "#511111" }}>
-                <h1>{count}</h1>
-                <button onClick={increaseCount}>increase</button><br/><br/>
-                <button onClick={decreaseCount}>decrease</button><br/><br/><button onClick={resetCount}>reset</button>
-            </div>
+            <button onClick={handleStateChange}>
+                {state.loading ? "Loading..." : "fetch cat fact"}
+            </button>
+            {state.fact && <h2>{state.fact}</h2>}
+            {state.error && <h2>Error. some thing is wrong</h2>}
         </div>
     );
 }
-
-function App() {
-    return (
-        <QueryClientProvider client={client}>
-            <AppContent />
-        </QueryClientProvider>
-    );
-}
-
 
 export default App;
---
+++ src/useReducerHook/useReducer.js
@@ -0,0 +1,26 @@
+export const initialState = {
+    fact : "",
+    loading: false,
+    error: false,
+}
+
+// fetch_start      : loading :true
+
+// fetch_success    : loading : false
+//                  : fact : res.data
+
+// fetch_error      : error : true
+//                  : loading : false
+
+export const handleState = (state = initialState, action) => {
+    switch (action.type) {
+        case "fetch_start":
+            return {...state ,loading: true, error: false, fact: "" }
+        case "fetch_success":
+            return {...state, loading: false, fact : action.fact, error: false}
+        case "fetch_error":
+            return {...state, loading: false, error: true , fact: "" }
+        default:
+            return {...state}
+    }
+}
